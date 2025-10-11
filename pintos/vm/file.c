09b3763d43d5d8a0b8bd2acfa85062006a84fbda
seongwonjung <jsjsw0918@naver.com>
2025-10-11 20:47:02 +0900

    feat: do_mmap 함수 구현

--- pintos/vm/file.c
+++ pintos/vm/file.c
@@ -1,7 +1,8 @@
 /* file.c: Implementation of memory backed file object (mmaped object). */
 
 #include "vm/vm.h"
-
+#include "threads/vaddr.h"
+#include "userprog/process.h"
 static bool file_backed_swap_in(struct page *page, void *kva);
 static bool file_backed_swap_out(struct page *page);
 static void file_backed_destroy(struct page *page);
@@ -49,6 +50,32 @@ static void file_backed_destroy(struct page *page)
 /* Do the mmap */
 void *do_mmap(void *addr, size_t length, int writable, struct file *file, off_t offset)
 {
+    struct thread *cur = thread_current();
+
+    uintptr_t upage = (uintptr_t)addr;
+    size_t remaining_bytes = length;
+    while (upage < (uintptr_t)addr + length)
+    {
+        size_t page_read_bytes = remaining_bytes < PGSIZE ? remaining_bytes : PGSIZE;
+        size_t page_zero_bytes = PGSIZE - page_read_bytes;
+
+        struct segment_info *aux = (struct segment_info *)malloc(sizeof(struct segment_info));
+        aux->file = file_reopen(file);
+        aux->ofs = offset;
+        aux->read_byte = page_read_bytes;
+        aux->zero_byte = page_zero_bytes;
+
+        if (!vm_alloc_page_with_initializer(VM_FILE, upage, writable, lazy_load_segment, aux))
+        {
+            file_close(aux->file);
+            free(aux);
+            return false;
+        }
+        /* Advance. */
+        remaining_bytes -= page_read_bytes;
+        offset += page_read_bytes;
+        upage += PGSIZE;
+    }
 }
 
 /* Do the munmap */
